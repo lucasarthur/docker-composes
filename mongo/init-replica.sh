bb45ca552e1881d12cf1b15f0ec61684e998eb90
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-18 01:33:45 -0300

    enhance init mongo scripts

--- mongo/init-replica.sh
+++ mongo/init-replica.sh
@@ -11,5 +11,11 @@ RS_MEMBER_1="{ \"_id\": 0, \"host\": \"${LOCAL_HOST}:${DB1_PORT}\", \"priority\"
 RS_MEMBER_2="{ \"_id\": 1, \"host\": \"${LOCAL_HOST}:${DB2_PORT}\", \"priority\": 0 }"
 RS_MEMBER_3="{ \"_id\": 2, \"host\": \"${LOCAL_HOST}:${DB3_PORT}\", \"priority\": 0 }"
 
-sleep 30
-mongosh --eval "rs.initiate({ \"_id\": \"${REPLICA_SET}\", \"members\": [${RS_MEMBER_1}, ${RS_MEMBER_2}, ${RS_MEMBER_3}] });"
+until echo $DB1_PORT $DB2_PORT $DB3_PORT | xargs -n1 nc -z $LOCAL_HOST
+do sleep .02
+done
+
+mongosh --eval "rs.status()" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  mongosh --eval "rs.initiate({ \"_id\": \"${REPLICA_SET}\", \"members\": [${RS_MEMBER_1}, ${RS_MEMBER_2}, ${RS_MEMBER_3}] });"
+fi
