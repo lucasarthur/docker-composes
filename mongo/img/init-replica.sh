604b807f96656c851955466d103e0d7153852212
Lucas Arthur <luca.oetdbem@gmail.com>
2023-06-08 05:43:43 -0300

    feat: 🎸 `nc` -> `wait-for-it` | try-catch rs init/telemetry off

--- mongo/img/init-replica.sh
+++ mongo/img/init-replica.sh
@@ -4,11 +4,24 @@ set -e
 LOCAL_HOST="${HOST:-localhost}"
 REPLICA_SET="${REPLICA_SET_NAME:-rs0}"
 
-until echo 27017 27018 27019 | xargs -n1 nc -z "${LOCAL_HOST}"
-do sleep .02
-done
+wait-for-it "${LOCAL_HOST}:27017" -t 0 -q
+wait-for-it "${LOCAL_HOST}:27018" -t 0 -q
+wait-for-it "${LOCAL_HOST}:27019" -t 0 -q
 
-mongosh --eval "rs.status()" > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  mongosh --eval "rs.initiate({ \"_id\": \"${REPLICA_SET}\", \"members\": [{ \"_id\": 0, \"host\": \"${LOCAL_HOST}:27017\", \"priority\": 2 }, { \"_id\": 1, \"host\": \"${LOCAL_HOST}:27018\", \"priority\": 0 }, { \"_id\": 2, \"host\": \"${LOCAL_HOST}:27019\", \"priority\": 0 }] });"
-fi
+mongosh --eval "\
+  disableTelemetry(); \
+  try { \
+    rs.status(); \
+    print('Replica set up and running!'); \
+  } catch (e) { \
+    print('Initializing replica set...'); \
+    rs.initiate({ \
+      '_id': '${REPLICA_SET}', \
+      'members': [ \
+        { '_id': 0, 'host': '${LOCAL_HOST}:27017', 'priority': 2 }, \
+        { '_id': 1, 'host': '${LOCAL_HOST}:27018', 'priority': 0 }, \
+        { '_id': 2, 'host': '${LOCAL_HOST}:27019', 'priority': 0 } \
+      ] \
+    }); \
+    print('Replica set up and running!'); \
+  }"
